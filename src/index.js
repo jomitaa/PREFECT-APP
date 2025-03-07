a4ced380c5aff9ad02cda786c07f2028e8a22a0d
pepin <pepin@MacBook-Air-de-Alicia.local>
2025-03-06 22:43:27 -0600

    pruebas

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ conexion.getConnection(err => {
     if (err) {
         console.error('Error conectando a la base de datos:', err);
     } else {
-        console.log('Conectado a MySQL en Railway');
+        console.log('Conectado a MySQL con Railway');
     }
 });
 
