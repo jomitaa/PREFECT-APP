ec8fb29d097fa5e6559a13ac302bb13349f352ca
Kiev07 <warlokk07@gmail.com>
2025-07-12 20:42:38 -0600

    zzz

--- src/public/ADM_Consulta.js
+++ src/public/ADM_Consulta.js
@@ -10,10 +10,7 @@ registroAsistenciaSeleccionado = '';
 
 
 
-<<<<<<< Updated upstream
-=======
 // Función para mostrar/ocultar el spinner
->>>>>>> Stashed changes
 function showLoading(show) {
   if (show) {
     const overlay = document.createElement('div');
